407360cc74349d3c6cb9912369603e581105f999
RaresSanduConstantin <59281487+RaresSanduConstantin@users.noreply.github.com>
2020-05-23 09:56:50 +0300

    Updates

---
+++ precache-manifest.f60c5f450be8936ab937063d8b7940bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd25affa7e9487f1d1acefbc22a2f850",
+    "url": "/cats-rolodex/index.html"
+  },
+  {
+    "revision": "1ba5b72c744ac40d9819",
+    "url": "/cats-rolodex/static/css/main.9a2cc582.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/cats-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cats-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ba5b72c744ac40d9819",
+    "url": "/cats-rolodex/static/js/main.bd94a5f1.chunk.js"
+  },
+  {
+    "revision": "f3619f5b32a2d9ecc6f2",
+    "url": "/cats-rolodex/static/js/runtime-main.f5fe2f9e.js"
+  }
+]);
