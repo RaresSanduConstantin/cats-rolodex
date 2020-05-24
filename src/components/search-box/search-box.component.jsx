efd7cc35b92da0f6bdaa1343880309ace894f0e4
RaresSanduConstantin <59281487+RaresSanduConstantin@users.noreply.github.com>
2020-05-24 18:42:55 +0300

    search bar

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import "./search-box.styles.css";
 
 export const SearchBox = ({ placeholder, handleChange }) => (
-  <input
-    className="search"
-    type="search"
-    placeholder={placeholder}
-    onChange={handleChange}
-  />
+  <div className="wrap">
+    {" "}
+    <div className="search">
+      <input
+        className="searchTerm"
+        type="search"
+        placeholder={placeholder}
+        onChange={handleChange}
+      />
+    </div>
+  </div>
 );
